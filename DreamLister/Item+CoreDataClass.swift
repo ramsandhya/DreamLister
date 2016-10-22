610f4762d5d128edd59af5694160480501d97a39
ramsandhya <ram.sandhya@gmail.com>
2016-10-21 23:32:22 -0400

    Created data model

---
+++ DreamLister/Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Jitendra Ram on 10/21/16.
+//  Copyright © 2016 Sandhya Ram. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Item: NSManagedObject {
+
+}
