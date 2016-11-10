69d6d2c4a0bbc070fa5172d6b8755c8b470226e5
ramsandhya <ram.sandhya@gmail.com>
2016-11-09 22:29:26 -0500

    Implemented the editing feature

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -44,6 +44,22 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         cell.configureCell(item: item)
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let objs = controller.fetchedObjects, objs.count > 0 {
+            let item = objs[indexPath.row]
+            performSegue(withIdentifier: "ItemDetailsVC", sender: item)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ItemDetailsVC" {
+            if let destination = segue.destination as? ItemDetailsVC {
+                if let item = sender as? Item {
+                    destination.itemToEdit = item
+                }
+            }
+        }
+    }
     
     func numberOfSections(in tableView: UITableView) -> Int {
         if let sections = controller.sections {
